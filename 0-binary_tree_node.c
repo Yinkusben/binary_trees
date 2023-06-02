ebedf8a03e14508d821ffbeb8bf18e98893941fd
Yinkusben <yinkusben4sure@gmail.com>
2023-06-02 15:00:30 +0000

    fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,8 +14,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = (binary_tree_t *) malloc(sizeof(binary_tree_t));
 
+	if (newNode == NULL)
+		return (NULL);
+
 	newNode->n = value;
 	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
 
 	return (newNode);
 }
