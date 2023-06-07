5fbef4752c040da716b1aa828c9e22f858e509d8
Yinkusben <yinkusben4sure@gmail.com>
2023-06-07 10:55:41 +0000

    test

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,16 +1,21 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * binary_tree_is_leaf - Checks if a node is a leaf of a binary tree.
- * @node: A pointer to the node to check.
+ * binary_tree_is_leaf - A function that checks if a mode is leaf.
  *
- * Return: If the node is a leaf - 1.
- *         Otherwise - 0.
+ * @node: A pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0
  */
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || node->left != NULL || node->right != NULL)
+	if (node == NULL)
 		return (0);
 
-	return (1);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
